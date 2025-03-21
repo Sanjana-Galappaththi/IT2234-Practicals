0701654af7a3529bdc88c48bd499b4db7be93dff
Sanjana-Galappaththi <sanjanagalappaththi456@gmail.com>
2025-03-21 21:27:04 +0530

    Add files via upload

---
+++ Day04/Note1.js
@@ -0,0 +1,18 @@
+const {createServer} = require('node:http');
+
+const localhost = '127.0.0.1';
+const port=3000;
+
+const server = createServer((req,res) =>
+{
+    res.sratusCode=200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end("Hello Node JS");
+
+});
+
+server.listen(port,localhost,()=>
+{
+    console.log(`Running on: ${localhost}:${port}`);
+})
+
